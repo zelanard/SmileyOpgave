4b3c5725da6d961fd868f22cb22d6c2296a1746d
Jonas Offersen <72074478+zelanard@users.noreply.github.com>
2025-11-19 08:37:14 +0100

    Json Basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "Json.h"
 
 // WiFi credentials
-const char* ssid = "IoT_H3/4_5G";
-const char* password = "98806829";
+const char *ssid = "IoT_H3/4_5G";
+const char *password = "98806829";
 
 // MQTT broker settings
-const char* mqtt_server = "wilson.local";
+const char *mqtt_server = "wilson.local";
 const int mqtt_port = 1883;
-const char* mqtt_topic = "esp32/tatiana_jonas/button";
+const char *mqtt_topic = "esp32/tatiana_jonas/button";
 
 const int LED_RED = 5;
 const int LED_BLUE = 19;
@@ -22,8 +22,24 @@ const int BUTTON_BLUE = 34;
 const int BUTTON_GREEN = 32;
 const int BUTTON_YELLOW = 33;
 
-void setup() {
+void setup()
+{
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+
+    pinMode(BUTTON_RED, INPUT_PULLUP);
+    pinMode(BUTTON_BLUE, INPUT_PULLUP);
+    pinMode(BUTTON_GREEN, INPUT_PULLUP);
+    pinMode(BUTTON_YELLOW, INPUT_PULLUP);
+
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_YELLOW, HIGH);
 }
 
-void loop() {
+void loop()
+{
 }
